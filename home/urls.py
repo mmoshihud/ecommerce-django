a0a06e97160383565c3b4434cdcb21185ee47f72
Munshi Moshihud Hossain <18-39125-3@student.aiub.edu>
2023-07-13 04:29:37 +0600

    login api

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
-    path('user/signup', views.handleSignup)
+    path('user/signup', views.handleSignup),
+    path('user/login', views.handleLogin)
 ]
--- home/views.py
+++ home/views.py
@@ -1,15 +1,37 @@
 from django.http import JsonResponse, HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 
 def handleSignup(request):
     if request.method == 'POST':
-        user_data = request.POST
-        print(user_data)
-
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
         user = User.objects.create_user(
-            username=user_data['username'], email=user_data['email'], password=user_data['password'])
+            username=username, email=email, password=password)
         user.save()
         return JsonResponse({'message': 'User created successfully'})
     else:
         HttpResponse('404 Not Found')
+
+
+def handleLogin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+    try:
+        user = User.objects.get(username=username)
+        if user.check_password(password):
+            # Password is correct
+            # Perform any additional actions if needed
+            return HttpResponse('User exists and password is correct!')
+        else:
+            # Password is incorrect
+            # Perform any additional actions if needed
+            return HttpResponse('Invalid login credentials!')
+    except User.DoesNotExist:
+        # User does not exist
+        # Perform any additional actions if needed
+        return HttpResponse('Invalid login credentials!')
